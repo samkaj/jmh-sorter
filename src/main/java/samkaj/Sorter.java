072a255e6a449e9b888511d0e771682a06b3920c
samkaj <kajavasamuel@gmail.com>
2023-05-12 14:00:12 +0200

    Merge sort

--- src/main/java/samkaj/Sorter.java
+++ src/main/java/samkaj/Sorter.java
@@ -7,23 +7,44 @@ public class Sorter {
    */
   public int[] sort(int[] toSort) {
     int[] sorted = toSort.clone();
-    if (sorted.length < 1)
-      return sorted;
-     
-    for (int i = 0; i < sorted.length - 1; i++) {
-      for (int j = 0; j < sorted.length - i - 1; j++) {
-        if (sorted[j] > toSort[j + 1]) {
-          swap(sorted[j], toSort[j+1]); 
-        }
-      }
+    mergeSort(sorted, 0, sorted.length - 1);
+    return sorted;
+  }
+
+  static void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[] = new int[n1];
+    int R[] = new int[n2];
+
+    for (int i = 0; i < n1; ++i) {
+      L[i] = arr[l + i];
     }
 
-    return sorted;
-  } 
+    for (int j = 0; j < n2; ++j) {
+      R[j] = arr[m + 1 + j];
+    }
 
-  private void swap(int x, int y) {
-    int tmp = x;
-    x = y;
-    y = tmp;
+    int i = 0, j = 0;
+    int k = l;
+    while (i < n1 && j < n2) {
+      if (L[i] <= R[j]) {
+        arr[k] = L[i];
+        i++;
+      } else {
+        arr[k] = R[j];
+        j++;
+      }
+      k++;
+    }
+  }
+
+  static void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+      int m = l + (r - l) / 2;
+      mergeSort(arr, l, m);
+      mergeSort(arr, m + 1, r);
+      merge(arr, l, m, r);
+    }
   }
 }
